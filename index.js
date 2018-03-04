3e0d448a645191365d92c7e1900db7f1de977250
Michael Lees <mlees15@student.sl.on.ca>
2018-03-04 14:29:30 -0500

    Added index, about, contact us pages

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.use(express.static(path.join(__dirname,"static")));
 app.get("/",(req,res)=>{
     res.sendFile("index.html");
 })
+app.get('/about',(req,res)=>{
+    res.sendFile(path.join(__dirname,'static/about.html'));
+})
+app.get('/contactUs',(req,res)=>{
+    res.sendFile(path.join(__dirname,"static/contactUs.html"))
+})
 app.listen(port,()=>{
     console.log("App is running on port " + port);
 })
