56dfefc9aa22f4430a333ab1eb30301a3310bd4e
Michael Lees <mlees15@student.sl.on.ca>
2018-03-04 14:04:46 -0500

    added index page

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const {port} = require('./config/env');
+const path = require('path');
 const app = express();
+
+app.use(express.static(path.join(__dirname,"static")));
+app.get("/",(req,res)=>{
+    res.sendFile("index.html");
+})
 app.listen(port,()=>{
     console.log("App is running on port " + port);
 })
